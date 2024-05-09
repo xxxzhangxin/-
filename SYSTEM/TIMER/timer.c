679afc5f14e56ff502933b24eb368e125395415a
xxxzhangxin <116888170+xxxzhangxin@users.noreply.github.com>
2024-05-09 11:15:09 +0800

    Add files via upload

---
+++ SYSTEM/TIMER/timer.c
@@ -0,0 +1,136 @@
+#include "timer.h"
+#include "stdio.h"
+#include "delay.h"
+/*安徽理工大学*/
+//////////////////////////////////////////////////////////////////////////////////	 
+//本程序只供学习使用，未经作者许可，不得用于其它任何用途
+//Mini STM32开发板
+//通用定时器 驱动代码			   
+//正点原子@ALIENTEK
+//技术论坛:www.openedv.com
+//修改日期:2010/12/03
+//版本：V1.0
+//版权所有，盗版必究。
+//Copyright(C) 正点原子 2009-2019
+//All rights reserved
+////////////////////////////////////////////////////////////////////////////////// 	  
+char oledBuf[20];
+extern u8 humidityH;	  //湿度整数部分
+extern u8 humidityL;	  //湿度小数部分
+extern u8 TemperatureH;   //温度整数部分
+extern u8 TemperatureL;   //温度小数部分
+extern u32  CO2Data;
+extern u32 TVOCData;
+
+extern u8 alarmFlag;//是否报警的标志
+
+//通用定时器中断初始化
+//这里时钟选择为APB1的2倍，而APB1为36M
+//arr：自动重装值。
+//psc：时钟预分频数
+//这里使用的是定时器3!
+void TIM3_Int_Init(u16 arr,u16 psc)
+{
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM3, ENABLE); //时钟使能
+
+	TIM_TimeBaseStructure.TIM_Period = arr; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值	 计数到5000为500ms
+	TIM_TimeBaseStructure.TIM_Prescaler =psc; //设置用来作为TIMx时钟频率除数的预分频值  10Khz的计数频率  
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM3, &TIM_TimeBaseStructure); //根据TIM_TimeBaseInitStruct中指定的参数初始化TIMx的时间基数单位
+ 
+	TIM_ITConfig(  //使能或者失能指定的TIM中断
+		TIM3, //TIM2
+		TIM_IT_Update ,
+		ENABLE  //使能
+		);
+	NVIC_InitStructure.NVIC_IRQChannel = TIM3_IRQn;  //TIM3中断
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //先占优先级0级
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //从优先级3级
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
+
+	TIM_Cmd(TIM3, ENABLE);  //使能TIMx外设
+							 
+}
+
+void TIM3_IRQHandler(void)   //TIM3中断
+{
+	if (TIM_GetITStatus(TIM3, TIM_IT_Update) != RESET) //检查指定的TIM中断发生与否:TIM 中断源 
+		{
+			TIM_ClearITPendingBit(TIM3, TIM_IT_Update  );  //清除TIMx的中断待处理位:TIM 中断源 
+			if(alarmFlag)//如果alarmFlag = 1 就执行报警
+			{
+				LED1=!LED1;
+				BEEP=!BEEP;
+			}
+			else
+			{
+				LED1=1;
+				BEEP=1;
+			}
+		}
+}
+
+void TIM2_Int_Init(u16 arr,u16 psc)
+{
+    TIM_TimeBaseInitTypeDef  TIM_TimeBaseStructure;
+	NVIC_InitTypeDef NVIC_InitStructure;
+
+	RCC_APB1PeriphClockCmd(RCC_APB1Periph_TIM2, ENABLE); //时钟使能
+
+	TIM_TimeBaseStructure.TIM_Period = arr; //设置在下一个更新事件装入活动的自动重装载寄存器周期的值	 计数到5000为500ms
+	TIM_TimeBaseStructure.TIM_Prescaler =psc; //设置用来作为TIMx时钟频率除数的预分频值  10Khz的计数频率  
+	TIM_TimeBaseStructure.TIM_ClockDivision = 0; //设置时钟分割:TDTS = Tck_tim
+	TIM_TimeBaseStructure.TIM_CounterMode = TIM_CounterMode_Up;  //TIM向上计数模式
+	TIM_TimeBaseInit(TIM2, &TIM_TimeBaseStructure); //根据TIM_TimeBaseInitStruct中指定的参数初始化TIMx的时间基数单位
+ 
+	TIM_ITConfig(  //使能或者失能指定的TIM中断
+		TIM2, //TIM2
+		TIM_IT_Update ,
+		ENABLE  //使能
+		);
+	NVIC_InitStructure.NVIC_IRQChannel = TIM2_IRQn;  //TIM2中断
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority = 0;  //先占优先级0级
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority = 3;  //从优先级3级
+	NVIC_InitStructure.NVIC_IRQChannelCmd = ENABLE; //IRQ通道被使能
+	NVIC_Init(&NVIC_InitStructure);  //根据NVIC_InitStruct中指定的参数初始化外设NVIC寄存器
+
+	TIM_Cmd(TIM2, ENABLE);  //使能TIMx外设
+							 
+}
+
+void TIM2_IRQHandler(void)   //TIM2中断
+{  
+	if (TIM_GetITStatus(TIM2, TIM_IT_Update) != RESET) //检查指定的TIM中断发生与否:TIM 中断源 
+		{
+			TIM_ClearITPendingBit(TIM2, TIM_IT_Update  );  //清除TIMx的中断待处理位:TIM 中断源 
+			
+				
+				sprintf(oledBuf,"T:%d.%d %%",TemperatureH,TemperatureL);
+				OLED_ShowString(0,0,(u8*)oledBuf,8,1);//8*16 “ABC”
+				sprintf(oledBuf,"C:%d ", CO2Data);
+				OLED_ShowString(46,0,(u8*)oledBuf,8,1);//8*16 “ABC”
+				sprintf(oledBuf,"O:%d",TVOCData);
+				OLED_ShowString(92,0,(u8*)oledBuf,8,1);//8*16 “ABC”
+			    OLED_drawCurve(TemperatureH*10+TemperatureL);
+			    
+			
+			
+				OLED_Refresh();
+			
+		}
+}
+
+
+
+
+
+
+
+
+
+
